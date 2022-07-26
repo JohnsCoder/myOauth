1cc4008a37453d67505d7b30bff5301dfa25b676
JohnsCoder <joaopdossantos1515@gmail.com>
2022-07-26 16:23:58 -0300

    fixing routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
 import "font-awesome/css/font-awesome.min.css";
-import { BrowserRouter } from "react-router-dom";
-import RegisterProvider from "./contexts/registerContext";
-
-import Paths from "./paths";
-import LoginProvider from "./contexts/loginContexts";
-import TodosProvider from "./contexts/todosContexts";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <BrowserRouter>
-      <React.StrictMode>
-        <RegisterProvider>
-          <LoginProvider>
-            <TodosProvider>
-              <Paths />
-            </TodosProvider>
-          </LoginProvider>
-        </RegisterProvider>
-      </React.StrictMode>
-    </BrowserRouter>
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
